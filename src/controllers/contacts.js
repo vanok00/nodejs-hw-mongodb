c2c07542218701e156be4ae6354c7aff8d1175d5
vanok00 <vanok008@ukr.net>
2025-01-19 18:30:48 +0200

    ctrlWrappr

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,25 +1,22 @@
 import { getAllContacts, getContactById } from "../services/contacts.js";
 
-export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+export const getContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
 
-  res.json({
-    status: 200,
-    message: "Mongo connection successfully established!",
-    data: contacts,
-  });
+    res.json({
+      status: 200,
+      message: "Mongo connection successfully established!",
+      data: contacts,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
-
-  //   if (!contact) {
-  //     res.status(404).json({
-  //       message: "Contact not found",
-  //     });
-  //     return;
-  //   }
 
   if (!contact) {
     next(new Error("Contact not found"));
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,10 +3,11 @@ import {
   getContactByIdController,
   getContactsController,
 } from "../controllers/contacts.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
 
-router.get("/contacts", getContactsController);
-router.get("/contacts/:contactId", getContactByIdController);
+router.get("/contacts", ctrlWrapper(getContactsController));
+router.get("/contacts/:contactId", ctrlWrapper(getContactByIdController));
 
 export default router;
