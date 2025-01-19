27deeb0ae0ea87707749c953c4b028c44153fb84
vanok00 <vanok008@ukr.net>
2025-01-19 18:45:27 +0200

    http-error

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
 import { getAllContacts, getContactById } from "../services/contacts.js";
+import createHttpError from "http-errors";
 
 export const getContactsController = async (req, res, next) => {
   try {
@@ -14,13 +15,12 @@ export const getContactsController = async (req, res, next) => {
   }
 };
 
-export const getContactByIdController = async (req, res, next) => {
+export const getContactByIdController = async (req, res) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    next(new Error("Contact not found"));
-    return;
+    throw createHttpError(404, "Contact not found");
   }
 
   res.json({
