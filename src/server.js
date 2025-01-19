db646f52aa49abe6ec8d4115c2cb58f273628fc0
vanok00 <vanok008@ukr.net>
2025-01-19 18:35:29 +0200

    errorHandlers

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from "pino-http";
 import cors from "cors";
 
 import contactsRouter from "./routers/contacts.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,18 +30,9 @@ export const setupServer = () => {
 
   app.use(contactsRouter);
 
-  app.use("*", (req, res, next) => {
-    res.status(404).json({
-      message: "Not found",
-    });
-  });
+  app.use("*", notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: "Something went wrong",
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
