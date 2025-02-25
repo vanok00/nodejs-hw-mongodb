9c315de061fd51c2efcb4d3e1d8f13ab5bd01d87
vanok00 <vanok008@ukr.net>
2025-02-25 21:42:21 +0200

    deleted-start-server

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,5 +1,5 @@
 import createHttpError from "http-errors";
-import swaggerUI from "swagger-ui-express";
+import swaggerUi from "swagger-ui-express";
 import fs from "node:fs";
 
 import { SWAGGER_PATH } from "../constants/index.js";
@@ -7,7 +7,7 @@ import { SWAGGER_PATH } from "../constants/index.js";
 export const swaggerDocs = () => {
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+    return [...swaggerUi.serve, swaggerUi.setup(swaggerDoc)];
   } catch (err) {
     return (req, res, next) => {
       next(createHttpError(500, "Can't load swagger docs"));
--- src/server.js
+++ src/server.js
@@ -11,28 +11,33 @@ import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 const PORT = Number(getEnvVar("PORT", "3001"));
 
-export const startServer = () => {
-  const app = express();
+// export const startServer = () => {
+//   const app = express();
 
-  app.use("/uploads", express.static(UPLOAD_DIR));
+//   app.use("/uploads", express.static(UPLOAD_DIR));
+//   app.use("/api-docs", swaggerDocs());
+//   app.use(express.json());
+//   app.use("/uploads", express.static(UPLOAD_DIR));
+//   app.use(cookieParser());
+//   app.use(
+//     cors({
+//       credentials: true,
+//     })
+//   );
+// };
+
+export const setupServer = () => {
+  const app = express();
   app.use("/api-docs", swaggerDocs());
-  app.use(express.json());
   app.use("/uploads", express.static(UPLOAD_DIR));
+  app.use(express.json());
+
   app.use(cookieParser());
   app.use(
     cors({
       credentials: true,
     })
   );
-};
-
-export const setupServer = () => {
-  const app = express();
-  app.use(cookieParser());
-
-  app.use(express.json());
-  app.use(cors());
-
   app.use(
     pino({
       transport: {
